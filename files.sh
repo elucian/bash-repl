ec5961505493180f63d4434e5854342e9f709a6b
Elucian Moise <elucian.moise@gmail.com>
2022-10-05 20:58:38 +0000

    improve example

--- files.sh
+++ files.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-for f in *.sh
+#list script files
+for fh in *.sh
 do
-  echo $f
-  printf '_%.0s' {1..30}
-  for line in f; do
-      echo line
-  done
+  printf '_%.0s' {1..40}
+  echo; echo "#file: $fh"
+  i=0
+  # display first 3 lines of code
+  while read -r line; do
+      echo $line
+      if (( i == 3 )); then break; fi
+      (( i++ ))
+  done < $PWD/$fh
 done
+printf '=%.0s' {1..40}; echo
