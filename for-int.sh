6315c2472da61c59eac5d9179fd8b6cedc306b0a
Elucian Moise <elucian.moise@gmail.com>
2022-10-05 19:04:58 +0000

    new example

---
+++ for-int.sh
@@ -0,0 +1,12 @@
+# For loop interuptions
+for (( i=0; i<=100; i++ ))
+do
+   # Output only even numbers
+   if ((  (( i % 2 ))  == 0 )); then
+      echo "Element $i"
+      continue
+   fi
+   if (( i > 20 )); then
+      break
+   fi
+done
