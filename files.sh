144ed699939ca9b7563091e7858e95e981e7916e
Elucian Moise <elucian.moise@gmail.com>
2022-10-05 21:08:41 +0000

    fix length of separator

--- files.sh
+++ files.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #list script files
-for fh in *.sh
+for fhnd in *.sh
 do
-  printf '_%.0s' {1..40}
-  echo; echo "#file: $fh"
+  printf '_%.0s' {1..30}
+  echo; echo "#file: $fhnd"
   i=0
   # display first 3 lines of code
   while read -r line; do
       echo $line
       if (( i == 3 )); then break; fi
       (( i++ ))
-  done < $PWD/$fh
+  done < $PWD/$fhnd
 done
-printf '=%.0s' {1..40}; echo
+printf '=%.0s' {1..30}; echo
